9298fcf57bf85f38a9b29f68f998c572d0762cd3
milanchheda <milan.chheda@gmail.com>
2018-06-08 15:23:56 +0400

    design updates

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,7 +4,7 @@
 <div class="flex">
     <div class="w-full md:mx-8 bg-white px-8 py-6 rounded shadow mb-16">
     	<h2 class="pb-4 text-red-dark uppercase font-semibold font-sans font-medium border-b-2 border-grey-light">Fixtures</h2>
-		<table class="text-left" style="border-collapse:collapse">
+		<table class="text-left w-full" style="border-collapse:collapse">
 			<thead>
 				<tr>
 					<th class="py-2 px-2 text-sm font-semibold font-sans font-medium uppercase bg-grey-lighter border-b border-grey-light">#</th>
