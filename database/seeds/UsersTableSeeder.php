25c41c878b79edecc0c5a029ed9deb4e9a396201
milanchheda <milan.chheda@gmail.com>
2018-06-09 18:16:54 +0400

    Added players autocomplete

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,20 +11,6 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => 'admin',
-            'email' => 'admin@dummy.com',
-            'password' => bcrypt('fifa@2018@123'),
-        ]);
-        DB::table('users')->insert([
-            'name' => 'nigel',
-            'email' => 'nigel@dummy.com',
-            'password' => bcrypt('nigel'),
-        ]);
-        DB::table('users')->insert([
-            'name' => 'ausaf',
-            'email' => 'ausaf@dummy.com',
-            'password' => bcrypt('ausaf'),
-        ]);
+
     }
 }
