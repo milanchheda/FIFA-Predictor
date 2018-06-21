82b67e0fc072fdbcc4da69749db5e5e7cb1741e6
milanchheda <milan.chheda@gmail.com>
2018-06-21 19:39:48 +0400

    fixed prediction lock time bug

--- resources/views/admin/predictions.blade.php
+++ resources/views/admin/predictions.blade.php
@@ -31,7 +31,10 @@
 						@if(isset($prediction->lock_time))
 							<?php
 								$now = Carbon\Carbon::now();
-								echo $length = Carbon\Carbon::parse($prediction->lock_time)->diffForHumans($now, false, false, 6);
+								if($now > $prediction->lock_time)
+									echo $length = '-';
+								else
+									echo $length = Carbon\Carbon::parse($prediction->lock_time)->diffForHumans($now, false, false, 6);
 							?>
 						@else
 							<?php echo $length = '-'; ?>
