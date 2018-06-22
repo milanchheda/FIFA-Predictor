2196bf75120d978794b9c46db986c1364764c695
milanchheda <milan.chheda@gmail.com>
2018-06-22 09:08:21 +0400

    fixed prediction lock time bug

--- resources/views/admin/predictions.blade.php
+++ resources/views/admin/predictions.blade.php
@@ -60,7 +60,7 @@
 									<input type="text" name="userPredictedPlayerId" class="players border-grey-light border p-2 w-48" value="{{ isset($user_predictions[$prediction->id]) ? $players[$user_predictions[$prediction->id]] : '' }}" />
 								@endif
 								@else
-									{{ isset($teams[$user_predictions[$prediction->id]]) ? $teams[$user_predictions[$prediction->id]] : 'Wait until time remaining appears.' }}
+									{{ isset($user_predictions[$prediction->id]) ? $teams[$user_predictions[$prediction->id]] : 'Wait until time remaining appears.' }}
 								@endif
 						@else
 							@if($prediction->type == 'overall')
